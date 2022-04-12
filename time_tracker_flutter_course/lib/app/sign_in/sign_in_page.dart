cb055dcc3670a135c9a24a82f615b04aa24d6440
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-12 12:42:42 -0500

    Agregar carpeta del proyecto principal del curso
    - Agregar los archivos de la UI
    - Agregar las dependencias necesarias para conexión con Firebase
    - Habilitar la conexión con Firebase desde la app de Flutter

---
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,97 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
+
+class SignInPage extends StatelessWidget {
+  Future<void> _signInAnonymously() async {
+    /*
+      Usamos el patrón de diseño Singleton, el cual es usado en POO con la 
+      finalidad de usar no más de una instancia de una clase. Además provee
+      acceso global a los recursos
+    */
+    // Retorna un Future<UserCredencial>
+    final userCredentials = await FirebaseAuth.instance.signInAnonymously();
+    print('${userCredentials.user.uid}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 4.0,
+      ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+// Se retorna un Widget, ya que Container hereda de Widget, así que no hay inconveniente.
+// Para hacer MÉTODOS PRIVADOS se debe anteponer "_" al nombre del método, los
+// cuales solo son accesibles dentro de la clase que lo contiene
+  Widget _buildContent() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      // Un child puede tener un Widget dentro de sí mismo.
+      child: Column(
+        // Para distribuir el contenido a lo largo del container.
+        mainAxisAlignment: MainAxisAlignment.center,
+        // Para ocupar todo el ancho del container.
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign in',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 48.0),
+          SocialSignInButton(
+            assetName: 'images/google-logo.png',
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SocialSignInButton(
+            assetName: 'images/facebook-logo.png',
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'or',
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.black87,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            // 'onPressed' es un callback, y no es necesario agregar (), pues no toma argumentos
+            onPressed: _signInAnonymously,
+          ),
+        ],
+      ),
+    );
+  }
+}
