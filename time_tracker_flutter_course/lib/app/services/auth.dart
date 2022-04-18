9f1a6730cd7010b7b2fbc36a3ea9c46583b4d199
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-18 16:15:21 -0500

    Agregar dependencias +  sign in con Google in auth.dart

--- time_tracker_flutter_course/lib/app/services/auth.dart
+++ time_tracker_flutter_course/lib/app/services/auth.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
   User? get currentUser;
   Stream<User?> authStateChanges();
   Future<User?> signInAnonymously();
+  Future<User?> signInWithGoogle();
   Future<void> signOut();
 }
 
@@ -19,12 +21,45 @@ class Auth implements AuthBase {
   @override
   User? get currentUser => _firebaseAuth.currentUser;
 
+  // Inicio de sesión anónimo
   @override
   Future<User?> signInAnonymously() async {
     final userCredential = await _firebaseAuth.signInAnonymously();
     return userCredential.user;
   }
 
+  // Inicio de sesión con Google
+  @override
+  Future<User?> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuth.signInWithCredential(
+          GoogleAuthProvider.credential(
+            idToken: googleAuth.idToken,
+            accessToken: googleAuth.accessToken,
+          ),
+        );
+        return userCredential.user;
+      } else {
+        // Si hay un error con el token id de Google:
+        throw FirebaseAuthException(
+          code: 'ERROR_MISSING_GOOGLE_ID_TOKEN',
+          message: 'Missing Google ID Token',
+        );
+      }
+    } else {
+      // Error: si no es posible iniciar sesión con Google
+      throw FirebaseAuthException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
+  }
+
+  // Cerrar sesión
   @override
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
