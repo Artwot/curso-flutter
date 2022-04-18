794bf18337ba1d4e56f085c334747f472dfd7a6d
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-18 14:25:41 -0500

    Agregar método en auth.dart para uso con Stream

--- time_tracker_flutter_course/lib/app/services/auth.dart
+++ time_tracker_flutter_course/lib/app/services/auth.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class AuthBase {
   User? get currentUser;
+  Stream<User?> authStateChanges();
   Future<User?> signInAnonymously();
   Future<void> signOut();
 }
@@ -9,6 +10,11 @@ abstract class AuthBase {
 // Dependency Injection
 class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
+
+  // Crear un Stream
+  // Notifica cambios en el estado de la sesión del usuario
+  @override
+  Stream<User?> authStateChanges() => _firebaseAuth.authStateChanges();
 
   @override
   User? get currentUser => _firebaseAuth.currentUser;
