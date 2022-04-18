8ba6211f4071abfee68e198dfcc49398c9e9d633
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-18 11:20:58 -0500

    Corregir la ruta de HomePage & LandingPage

--- time_tracker_flutter_course/lib/main.dart
+++ time_tracker_flutter_course/lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/services/auth.dart';
-import 'package:time_tracker_flutter_course/landing_page.dart';
+import 'package:time_tracker_flutter_course/app/landing_page.dart';
 
 // Definir el método principal de la aplicación
 Future<void> main() async {
