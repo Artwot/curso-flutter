b83a91208acc0d9e68fe1e988403e97f5c8e421e
Jorge Sarabia <jsarabiar98@gmail.com>
2022-04-26 12:40:42 -0500

    Refactorizar código de SignInBloc

--- time_tracker_flutter_course/lib/app/sign_in/sign_in_bloc.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_bloc.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../services/auth.dart';
 
 class SignInBloc {
+  SignInBloc({required this.auth});
+  final AuthBase auth;
   final StreamController<bool> _isLoadingController = StreamController<bool>();
   // Tiene acceso al Stream pero no al controller
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
@@ -11,5 +15,26 @@ class SignInBloc {
     _isLoadingController.close();
   }
 
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+  // Podemos pasar funciones como argumentos a otras funciones
+  Future<User?> _signIn(Future<User?> Function() signInMethod) async {
+    try {
+      _setIsLoading(true);
+      return await signInMethod();
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setIsLoading(false);
+    }
+  }
+
+  Future<User?> signInAnonymously() async =>
+      await _signIn(auth.signInAnonymously);
+
+  Future<User?> signInWithGoogle() async =>
+      await _signIn(auth.signInWithGoogle);
+
+  Future<User?> signInWithFacebook() async =>
+      await _signIn(auth.signInWithFacebook);
 }
